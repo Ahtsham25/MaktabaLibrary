35392401f921434e96ed9f84e967924cbaa3f5a4
Ahtsham25 <princeshami365@gmail.com>
2026-08-07 19:23:23 +0500

    Refactor AuthorAdapter to use new view elements

--- app/src/main/java/com/shamiacademy/maktabalibrary/adapter/AuthorAdapter.kt
+++ app/src/main/java/com/shamiacademy/maktabalibrary/adapter/AuthorAdapter.kt
@@ -3,8 +3,6 @@ package com.shamiacademy.maktabalibrary.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.shamiacademy.maktabalibrary.R
@@ -16,8 +14,9 @@ class AuthorAdapter(
 ) : RecyclerView.Adapter<AuthorAdapter.VH>() {
 
     class VH(view: View) : RecyclerView.ViewHolder(view) {
-        val image: ImageView = view.findViewById(R.id.image_cover)
-        val title: TextView = view.findViewById(R.id.text_title)
+        val image: android.widget.ImageView = view.findViewById(R.id.image_cover)
+        val name: android.widget.TextView = view.findViewById(R.id.text_name)
+        val count: android.widget.TextView = view.findViewById(R.id.text_count)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
@@ -27,8 +26,11 @@ class AuthorAdapter(
 
     override fun onBindViewHolder(holder: VH, position: Int) {
         val item = items[position]
-        holder.title.text = item.nameUr
-
+        
+        // nameUr اور bookCount نل ہونے پر بھی کریش نہیں ہوگا
+        holder.name.text = item.nameUr ?: "نامعلوم مصنف"
+        holder.count.text = "${item.bookCount ?: 0} کتابیں"
+        
         Glide.with(holder.image.context)
             .load(item.coverImageUrl)
             .placeholder(R.color.surface_black)
@@ -38,7 +40,7 @@ class AuthorAdapter(
         holder.itemView.setOnClickListener { onClick(item) }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount() = items.size
 
     fun updateData(newItems: List<Maktaba>) {
         items = newItems
